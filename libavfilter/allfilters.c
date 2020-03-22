3be1613197e6f9786b1132bffcf7de442ceca4ff
zzzworm <zzzworm@gmail.com>
2020-03-22 15:34:17 +0800

    add wave filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -477,6 +477,7 @@ extern AVFilter ff_avf_showspectrum;
 extern AVFilter ff_avf_showspectrumpic;
 extern AVFilter ff_avf_showvolume;
 extern AVFilter ff_avf_showwaves;
+extern AVFilter ff_avf_extshowwaves;
 extern AVFilter ff_avf_showwavespic;
 extern AVFilter ff_vaf_spectrumsynth;
 
